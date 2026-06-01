175841bf637de94a7a87aa2cab82972dc639cf61
Whale Choi <cj1369636717@gmail.com>
2026-06-02 01:26:10 +0800

    core: bump Xray-core to v26.6.1

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -13,8 +13,8 @@ object ProjectConfig {
     const val PROJECT_NAME = "AsteriskNG"
     const val VERSION_NAME = "1.2.1-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.ang"
-    const val XRAY_CORE_VERSION = "v26.5.9"
-    const val ANDROID_LIB_XRAY_LITE_VERSION = "v26.5.19"
+    const val XRAY_CORE_VERSION = "v26.6.1"
+    const val ANDROID_LIB_XRAY_LITE_VERSION = "v26.6.1"
     const val HEV_SOCKS5_TUNNEL_VERSION = "2.15.0"
     const val TARGET_SDK = 37
     const val MIN_SDK = 24
