fb616705a76ebc533f7ad5c06c39070660db6960
Whale Choi <cj1369636717@gmail.com>
2026-08-30 23:33:18 +0800

    fix: remove awk dependency from service control

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -11,9 +11,9 @@ import org.gradle.api.tasks.TaskAction
 object ProjectConfig {
     const val JVM_VERSION = 26
     const val PROJECT_NAME = "AsteriskNG"
-    const val VERSION_NAME = "1.4.5"
+    const val VERSION_NAME = "1.4.6-dev"
     const val PACKAGE_NAME = "org.asterisk.zcc.ang"
-    const val ASTERISKD_VERSION = "v2.0.24"
+    const val ASTERISKD_VERSION = "v2.0.25"
     const val BPF2SOCKS_VERSION = "v1.0.13"
     const val BPF_MATCHER_VERSION = "v1.0.1"
     const val XRAY_CORE_VERSION = "v26.7.28"
